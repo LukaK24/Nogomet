343cc48f1e30d5f7ea668c517c4099336a17a4b9
LukaK24 <lkordic234@gmail.com>
2025-02-06 11:09:37 +0100

    dodavanje alata

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,19 +8,44 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NogometContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NogometContext"));
+});
+
+//builder.Services.AddDbContext<InventorijaContext>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("InventorijaContext"),
+//        sqlOptions => sqlOptions.EnableRetryOnFailure())
+//);
+builder.Services.AddCors(o => {
+
+    o.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.MapOpenApi();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseSwagger();
+app.UseSwaggerUI(o =>
+{
+    o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+});
+
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
