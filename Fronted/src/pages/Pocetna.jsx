6d2d40133c5de350c2982c82adcce78d5d2742a9
LukaK24 <lkordic234@gmail.com>
2025-03-08 10:53:16 +0100

    fronted

--- Fronted/src/pages/Pocetna.jsx
+++ Fronted/src/pages/Pocetna.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
+import pozadina from '/images/pozadina.jpg'; // Import slike iz public/images
+
+
 
 const Pocetna = () => {
-  // Definiramo stilove s pozadinskom slikom
   const containerStyle = {
-    backgroundImage: 'url("/radnapozadina/slike")',  // Putanja do slike u public folderu
-    backgroundSize: 'cover',   // Slika će pokriti cijeli ekran
-    backgroundPosition: 'center',  // Slika će biti centrirana
-    height: '100vh',            // Visina cijelog ekrana
-    display: 'flex',            // Flexbox za centriranje sadržaja
-    justifyContent: 'center',  // Centriranje horizontalno
-    alignItems: 'center',      // Centriranje vertikalno
-    color: '#fff',             // Boja teksta
+    backgroundImage: `url(${pozadina})`, // Koristimo importiranu sliku
+    backgroundSize: 'contain', // Slika će se prilagoditi veličini elementa (neće biti izrezana)
+    backgroundPosition: 'center',
+    height: '85vh', // Visina elementa
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'black', // Postavljanje boje teksta na crnu
+    maxWidth: '90%', // Maksimalna širina 90% ekrana
+    wordWrap: 'break-word', // Ako je tekst predug, prelomit će se
   };
+
+  const headerStyle = {
+    position: 'absolute',   // Koristi apsolutnu poziciju
+    top: '5px',            // Pozicioniraj 10px od vrha
+    left: '55%',            // Centriraj horizont
+    transform: 'translateX(-50%)',  // Centriraj tačno iz sredine
+    zIndex: 2,              // Povećaj z-index da bi tekst bio iznad slike
+  };
+
+
+
 
   return (
     <div style={containerStyle}>
-      <h1>Dobrodošli na početnu stranicu!</h1>
+      <h1 style={headerStyle}>Dobrodošli na nogometnu stranicu</h1>
     </div>
   );
 };
 
 export default Pocetna;
+
+
+
+
