f176955b24591fab681a6fc4c66d163bb2c1845e
LukaK24 <lkordic234@gmail.com>
2025-03-08 10:08:31 +0100

    Frontend u produkciju

--- Fronted/src/pages/Pocetna.jsx
+++ Fronted/src/pages/Pocetna.jsx
@@ -1,10 +1,23 @@
+import React from 'react';
 
+const Pocetna = () => {
+  // Definiramo stilove s pozadinskom slikom
+  const containerStyle = {
+    backgroundImage: 'url("/radnapozadina/slike")',  // Putanja do slike u public folderu
+    backgroundSize: 'cover',   // Slika će pokriti cijeli ekran
+    backgroundPosition: 'center',  // Slika će biti centrirana
+    height: '100vh',            // Visina cijelog ekrana
+    display: 'flex',            // Flexbox za centriranje sadržaja
+    justifyContent: 'center',  // Centriranje horizontalno
+    alignItems: 'center',      // Centriranje vertikalno
+    color: '#fff',             // Boja teksta
+  };
 
-export default function Pocetna(){
-    return(
-      <>
-      Dobrodošli u nogometnu aplikaciju
-       </>
-    )
+  return (
+    <div style={containerStyle}>
+      <h1>Dobrodošli na početnu stranicu!</h1>
+    </div>
+  );
+};
 
-}
+export default Pocetna;
